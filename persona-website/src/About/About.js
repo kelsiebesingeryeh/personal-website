fd35d4023893b02251edcb9892edf586f7e9b5c0
Kelsie Besinger <kelsiebesinger@gmail.com>
2021-02-09 12:11:37 -0700

    Add about me section to website

--- persona-website/src/About/About.js
+++ persona-website/src/About/About.js
@@ -1,27 +1,23 @@
-import React from 'react'
-import './About.css'
-import headshot from '../assets//headshot/headshot.png'
+// import React from 'react'
+// import './About.css'
 
-const About = () => {
-    return (
-      <section className="aboutMe">
-          <div className='firstColumn'>
-            <img className='headShot' src={headshot} />
-          </div>
-          <div className='secondColumn'>
-            <h2>About Me</h2>
-            <p className='aboutOverview'>
-            I am a process-oriented thinker with over 8 years of experience as an
-            operations leader for some of the world’s largest tech conferences. I
-            am a well-rounded engineer who offers a lot of understanding about
-            business from making processes better on teams to ensuring workflows
-            are seamless. By applying my operational mindset along with my
-            problem-solving skills, I design clean and unique user interfaces
-            while architecting concise and maintainable code.
-            </p>
-          </div>
-      </section>
-    );
-}
+// const About = () => {
+//     return (
+//       <section className="aboutMe">
+//           <div className='secondColumn'>
+//             <h2>About Me</h2>
+//             <p className='aboutOverview'>
+//             I am a process-oriented thinker with over 8 years of experience as an
+//             operations leader for some of the world’s largest tech conferences. I
+//             am a well-rounded engineer who offers a lot of understanding about
+//             business from making processes better on teams to ensuring workflows
+//             are seamless. By applying my operational mindset along with my
+//             problem-solving skills, I design clean and unique user interfaces
+//             while architecting concise and maintainable code.
+//             </p>
+//           </div>
+//       </section>
+//     );
+// }
 
-export default About
+// export default About
---
+++ persona-website/src/Resume/Resume.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Resume = () => {
+    return (
+    <section className='resumeSection'>
+        //aboutOverview
+        // skills
+        //education
+        // 
+    </section>
+    )
+}
+
+export default Resume
