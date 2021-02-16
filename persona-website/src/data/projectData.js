c2da52337fafaa4273c7eec7ea72afeded33bbe3
Kelsie Besinger <kelsiebesinger@gmail.com>
2021-02-16 07:53:08 -0700

    Add data to display on projects page

--- persona-website/src/data/projectData.js
+++ persona-website/src/data/projectData.js
@@ -1,11 +1,42 @@
 const projectData = [
-    {
-        id: 1,
-        title: 'Rancid Tomatillos',
-        shortOverview: 'Test Test',
-        stack: ['JavaScript', 'SASS/CSS', 'webpack'],
-        description: 'Test Test Test Tes'
-    }
-]
+  {
+    id: 1,
+    title: "Rancid Tomatillos",
+    shortOverview: "Test Test",
+    stack: ["JavaScript", "React", "SASS", "REST APIs", "Cypress", "InVision"],
+    description:
+      "Built a multi-page movie application that allows users to search, select and view different movies using REST APIs",
+    gitHubRepo: "<link>",
+    liveDeployment: "<link>",
+  },
+  {
+    id: 2,
+    title: "Travel Tracker",
+    shortOverview: "Test Test",
+    stack: [
+      "JavaScript",
+      "HTML",
+      "SASS",
+      "REST APIs",
+      "Webpack",
+      "Mocha/Chai",
+      "InVision",
+    ],
+    description:
+      "Developed a travel tracking application that allows users to manage, track and book different trips using RESTful APIs",
+    gitHubRepo: "<link>",
+    liveDeployment: "<link>",
+  },
+  {
+    id: 3,
+    title: "FitLit",
+    shortOverview: "Test Test",
+    stack: ["JavaScript", "HTML", "CSS", "Chart.js", "Mocha/Chai"],
+    description:
+      "Created a fitness application that displayed a dashboard for users to see their latest activities",
+    gitHubRepo: "<link>",
+    liveDeployment: "<link>",
+  },
+];
 
 export default projectData
