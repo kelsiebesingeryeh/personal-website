b9db56713b42697114e26d56c1a583ae103ff632
Kelsie Besinger <kelsiebesinger@gmail.com>
2021-02-17 14:18:58 -0700

    Add tagline to homepage

--- persona-website/src/Home/Home.js
+++ persona-website/src/Home/Home.js
@@ -5,6 +5,7 @@ const Home = () => {
     return (
       <div className="content">
         <h1 className="h1Animation skateIn">Hey, I'm Kelsie</h1>
+        <p className='aboutMe'>I'm a Software Engineer relentlessly focused on building seamless frontend applications.</p>
       </div>
     );
 }
