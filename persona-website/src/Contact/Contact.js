01badf1d896a9631c0d889222dee2a30607b946c
Kelsie Besinger <kelsiebesinger@gmail.com>
2021-02-18 09:45:58 -0700

    change contact page

--- persona-website/src/Contact/Contact.js
+++ persona-website/src/Contact/Contact.js
@@ -6,9 +6,6 @@ const Contact = () => {
     <section className="contactPage">
       <h2 className="contactTitle">Let's Work Together</h2>
       <a href="mailto:kelsiebesinger@gmail.com" className="emailContact">SAY HELLO</a>
-      {/* <h3>Email</h3>
-      <h3>GitHub</h3>
-      <h3>LinkedIn</h3> */}
     </section>
 
     )
