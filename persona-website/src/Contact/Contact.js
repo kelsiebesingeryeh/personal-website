2929dadd8eea8360ffb80eb7fd98ad385d49bb58
Kelsie Besinger <kelsiebesinger@gmail.com>
2021-02-18 10:12:19 -0700

    Finish contact page

--- persona-website/src/Contact/Contact.js
+++ persona-website/src/Contact/Contact.js
@@ -3,11 +3,16 @@ import './Contact.css'
 
 const Contact = () => {
     return (
-    <section className="contactPage">
-      <h2 className="contactTitle">Let's Work Together</h2>
-      <a href="mailto:kelsiebesinger@gmail.com" className="emailContact">SAY HELLO</a>
-    </section>
-
-    )
+      <div>
+        <section className="contactPage content">
+          <h1 className="contactTitle h1Animation skateIn">
+            Let's Work Together
+          </h1>
+          <a href="mailto:kelsiebesinger@gmail.com" className="emailContact">
+            SAY HELLO
+          </a>
+        </section>
+      </div>
+    );
 }
 export default Contact
