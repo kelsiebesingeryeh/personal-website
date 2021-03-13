95d86e6e189836b0ca805e9525de6af7e7be91f3
Kelsie Besinger <kelsiebesinger@gmail.com>
2021-03-13 12:55:04 -0700

    Fix responsiveness in projects page

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -18,37 +18,35 @@ const stackSentence = stack.join(", ");
       <div className="cardContainer">
         <p className="projectTitle">{title}</p>
         <div className="projectDetailsCard">
-          <div className="imageContainer">
             <img
               src={image}
               alt="travelTrackerScreenShot"
               className="projectImage"
             ></img>
-          </div>
-          <p>{shortOverview}</p>
-          <p>
-            <strong>Tech Stack:</strong> {stackSentence}
-          </p>
-          <p>{description}</p>
-          {title === "Travel Tracker" && (
-            <p>
-              <em>Login Information:</em>
-              <br></br>
-              <strong>username:</strong> traveler5<br></br>
-              <strong> password:</strong> traveler2020
+            <p className="projectDetails">{shortOverview}</p>
+            <p className="projectDetails">
+              <strong>Tech Stack:</strong> {stackSentence}
             </p>
+            <p className="projectDetails">{description}</p>
+            {title === "Travel Tracker" && (
+              <p>
+                <em>Login Information:</em>
+                <br></br>
+                <strong>username:</strong> traveler5<br></br>
+                <strong> password:</strong> traveler2020
+              </p>
           )}
-          <span className="gitHubLinks">
-            <a href={liveDeployment} className="projectLinks" target="_blank">
-              Live Deployment
-            </a>
-            <a href={gitHubRepo} className="projectLinks" target="_blank">
-              Git Repository
-            </a>
-          </span>
+            <span className="gitHubLinks">
+              <a href={liveDeployment} className="projectLinks" target="_blank">
+                Live Deployment
+              </a>
+              <a href={gitHubRepo} className="projectLinks" target="_blank">
+                Git Repository
+              </a>
+            </span>
         </div>
       </div>
     </>
-  )
+  );
 }
 export default Card
